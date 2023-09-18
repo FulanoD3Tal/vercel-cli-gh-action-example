c19a0cddb134004ead7bb8e28e2b9c8a440e7490
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2023-09-18 14:13:11 -0600

    feat: update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,45 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-2xl'>
-        Hi from <strong>feat/testing</strong>
-        By{' '}
-        <Image
-          src='/vercel.svg'
-          alt='Vercel Logo'
-          className='dark:invert'
-          width={100}
-          height={24}
-          priority
-        />
-      </h1>
-      <p>
-        Adipisicing eu irure velit ullamco sunt ea deserunt tempor labore. Esse
-        consectetur nulla pariatur sit nostrud velit in ipsum enim ut. Minim
-        esse fugiat laboris amet laborum duis minim.
-      </p>
-      <p>
-        Adipisicing eu irure velit ullamco sunt ea deserunt tempor labore. Esse
-        consectetur nulla pariatur sit nostrud velit in ipsum enim ut. Minim
-        esse fugiat laboris amet laborum duis minim.
-      </p>
-      <Image
-        className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-        src='/next.svg'
-        alt='Next.js Logo'
-        width={180}
-        height={37}
-        priority
-      />
-      By{' '}
-      <Image
-        src='/vercel.svg'
-        alt='Vercel Logo'
-        className='dark:invert'
-        width={100}
-        height={24}
-        priority
-      />
+      <h1 className='text-2xl'>Hi from new version</h1>
     </main>
   );
 }
